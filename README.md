af2042d98f452c430a41ce1f08dd7ba1768e7b01
DavidDGamble <daviddgamble@gmail.com>
2022-10-28 09:49:25 -0700

    Complete test toNumberArray() tested and passed and add numberSwap() to README and js. Tested and passed.

--- README.md
+++ README.md
@@ -12,3 +12,12 @@ Description: toNumberArray()
 Test: "Should take the input number and turn it into an array containing each number counting up to and including the input number starting at 0."
 Code: toNumberArray(5)
 Expected Output: [0, 1, 2, 3, 4, 5]
+
+Description: numberSwap()
+
+Test: "Should take input array of numbers and swap numbers containing 3 with "Won't you be my neighbor?", numbers containing 2 with "Boop!", and numbers containing 1 with "Beep!" in that heirarchy."
+Code: numberSwap([0, 1, 2, 3, 4, 5])
+Expected Output: 0, "Beep!", "Boop!", "Won't you be my neighbor?", 4, 5
+
+Code: numberSwap([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+Expected Output: 0, "Beep", "Boop!", "Won't you be my neighbor?", 4, 5, 6, 7, 8, 9, "Beep!", "Beep!", "Boop!", "Won't you be my neighbor?", "Beep!"
--- js/scripts.js
+++ js/scripts.js
@@ -12,8 +12,36 @@ function toNumberArray(number) {
   let i = 0;
   while (i <= num) {
     numArray.push(i);
+    i++;
   }
   return numArray;
 }
 
-console.log(toNumberArray(5));
+function numberSwap(numberArray) {
+  numberArray.forEach(function(element) {
+    let currentNumArray = element.toString().split('');
+    let has1 = false;
+    let has2 = false;
+    let has3 = false;
+    currentNumArray.forEach(function(element2) {
+      if (parseInt(element2) === 1) {
+        has1 = true;
+      } else if (parseInt(element2) === 2) {
+        has2 = true;
+      } else if (parseInt(element2) === 3) {
+        has3 = true;
+      }
+    });
+    if (has3) {
+      numberArray[element] = '"Won\'t you be my neighbor?"';
+    } else if (has2) {
+      numberArray[element] = '"Boop!"';
+    } else if (has1) {
+      numberArray[element] = '"Beep!"';
+    }
+  });
+  return numberArray.join(', ');
+} 
+
+console.log(numberSwap([0, 1, 2, 3, 4, 5]));
+console.log(numberSwap([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]));
